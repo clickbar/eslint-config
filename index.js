68542d3da8a764c92b48522b9b369db3f090a424
Alexander von Studnitz <alexander.von.studnitz@gmail.com>
2019-08-09 10:25:42 +0200

    Add rules

---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+    'cypress/globals': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:cypress/recommended',
+    'plugin:vue/recommended',
+    'prettier',
+    'prettier/vue',
+  ],
+  // required to lint *.vue files
+  plugins: ['cypress', 'vue', 'prettier'],
+  // add your custom rules here
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any',
+        },
+      },
+    ],
+    'prettier/prettier': 'error',
+  },
+}
