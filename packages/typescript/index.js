5420dd4c4fc2970243133d7a6fcefb2a1185bcc7
Alexander von Studnitz <studnitz@hey.com>
2022-01-19 10:57:05 +0100

    Update typescript lint

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -9,13 +9,18 @@ module.exports = {
   rules: {
     'import/named': 'off',
 
+    '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/consistent-type-imports': [
       'error',
       { prefer: 'type-imports', disallowTypeAnnotations: false },
     ],
-    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/prefer-literal-enum-member': 'error',
+    '@typescript-eslint/prefer-nullish-coalescing': 'warn',
+    '@typescript-eslint/prefer-optional-chain': 'warn',
+    '@typescript-eslint/prefer-readonly': 'warn',
+    '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+    '@typescript-eslint/prefer-ts-expect-error': 'error',
+    '@typescript-eslint/strict-boolean-expressions': 'warn',
   },
 }
