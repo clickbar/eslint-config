c1c081dc74b87bd8c2bf24a6f5d09dd7c05beb87
Alexander von Studnitz <studnitz@hey.com>
2022-01-20 11:19:46 +0100

    Add more Vue rules 🛰

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -14,23 +14,33 @@ module.exports = {
         project: ['./tsconfig.json'],
         extraFileExtensions: ['.vue'],
       },
-      rules: {
-        'no-unused-vars': 'off',
-        'no-undef': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
-      },
     },
   ],
   extends: ['plugin:vue/vue3-recommended', '@clickbar/eslint-config-typescript'],
   rules: {
     'vue/max-attributes-per-line': 'off',
     'vue/no-v-html': 'off',
+    'vue/block-lang': [
+      'error',
+      { 'script': { 'lang': 'ts' } },
+    ],
     'vue/component-api-style': [
       'error',
-      ['script-setup'], // "script-setup", "composition", "composition-vue2", or "options"
+      ['script-setup'], // 'script-setup', 'composition', 'composition-vue2', or 'options'
     ],
+    'vue/component-options-name-casing': ['error', 'PascalCase'],
     'vue/custom-event-name-casing': ['error', 'camelCase'],
     'vue/html-button-has-type': 'error',
+    'vue/next-tick-style': ['error', 'promise'],
+    'vue/no-empty-component-block': 'error',
+    'vue/no-expose-after-await': 'error',
+    'vue/no-multiple-objects-in-class': 'error',
+    'vue/no-template-target-blank': 'error',
+    'vue/no-useless-mustaches': 'error',
+    'vue/no-useless-v-bind': 'error',
+    'vue/padding-line-between-blocks': 'error',
+    'vue/prefer-separate-static-class': 'error',
+    'vue/v-for-delimiter-style': 'error',
     'vue/v-on-function-call': 'error',
   },
 }
