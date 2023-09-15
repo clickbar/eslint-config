61588d59361c13151644868fec09357ecf91b3c1
Tobias Kröll <tk@clickbar.dev>
2023-09-15 11:49:12 +0200

    fix: typescript-eslint/no-unused-vars being enabled
    Fixes #32

---
+++ .changeset/twenty-fireants-sneeze.md
@@ -0,0 +1,5 @@
+---
+'@clickbar/eslint-config-typescript': patch
+---
+
+Fix @typescript-eslint/no-unused-vars being enabled for ts files
--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -22,7 +22,8 @@ module.exports = {
         '@typescript-eslint/no-unsafe-call': 'warn',
         '@typescript-eslint/no-unsafe-member-access': 'warn',
         '@typescript-eslint/no-unsafe-return': 'warn',
-        '@typescript-eslint/no-unused-vars': 'warn',
+        // handled by unused-imports/no-unused-imports
+        '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/unified-signatures': 'warn',
       },
     },
